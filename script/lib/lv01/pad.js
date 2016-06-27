3ddc92ad48393c25ab35045ba56e58a2702d0ac7
Guilherme Vieira <super.driver.512@gmail.com>
2016-06-27 03:13:50 -0300

    Implement wgt.pad function.

---
+++ script/lib/lv01/pad.js
@@ -0,0 +1,32 @@
+'use strict'; {
+
+wgt.pad = (side, val, len, char_) => {
+    val = val.toString();
+
+    len -= val.length;
+
+    if(len <= 0) {
+        return val;
+    }
+
+    char_ = char_.toString().charAt(0);
+
+    let padStr = new Array(len + 1).join(char_);
+
+    switch(side) {
+        case 'left':
+            val = padStr + val;
+            break;
+
+        case 'right':
+            val += padStr;
+            break;
+
+        default:
+            throw new Error(`Unknown side: ${side}`);
+    }
+
+    return val;
+};
+
+}
