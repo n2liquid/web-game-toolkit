ab5ad2c5de7cfce8ba9b185e6ed1bd11ffe6caf9
Guilherme Vieira <super.driver.512@gmail.com>
2016-06-27 04:43:35 -0300

    Implement tilemap --wgtBgTileset{X,Y} CSS properties.

--- script/lib/lv02/tilemap.watcher.js
+++ script/lib/lv02/tilemap.watcher.js
@@ -18,7 +18,50 @@ wgt.watchDom({
                 wgt.fullUrl(`data/tileset/${tsFileName}.png`) +
             '")'
         );
-    }
+    },
+});
+
+wgt.watchDom({
+    added: true,
+    attr: 'style',
+    is: '.wgtTilemap',
+
+    fn: (el, ev) => {
+        let tsUrl = wgt.cssVar.get(el, 'wgtTilesetUrl');
+
+        let bgTsx = wgt.cssVar.get(el, 'wgtBgTilesetX', 'int');
+        let bgTsy = wgt.cssVar.get(el, 'wgtBgTilesetY', 'int');
+
+        if(
+            el.oldTsUrl === tsUrl
+            && el.oldBgTsx === bgTsx
+            && el.oldBgTsy === bgTsy
+        ) {
+            return;
+        }
+
+        el.oldTsUrl = tsUrl;
+        el.oldBgTsx = bgTsx;
+        el.oldBgTsy = bgTsy;
+
+        let $el = $(el);
+        let $wgt = $el.closest('.wgt');
+
+        let tw = wgt.cssVar.get($wgt[0], 'wgtTileWidth', 'int');
+        let th = wgt.cssVar.get($wgt[0], 'wgtTileHeight', 'int');
+
+        tsUrl = wgt.parseCssUrlValue(tsUrl);
+
+        wgt.cropTileset(tsUrl, tw, th, bgTsx, bgTsy).then(blob => {
+            let tObjUrl = URL.createObjectURL(blob);
+
+            $el.css('background-image', `url("${tObjUrl}")`);
+
+            setTimeout(() => {
+                URL.revokeObjectURL(tObjUrl);
+            }, 50);
+        }).done();
+    },
 });
 
 }
